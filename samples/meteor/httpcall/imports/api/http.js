2af3b72cd820fae153114390af50b7c695c45dd5
Phil <phil_colbert@hotmail.com>
2017-01-29 18:29:47 -0800

    Created

---
+++ samples/meteor/httpcall/imports/api/http.js
@@ -0,0 +1,38 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+
+if (Meteor.isServer) {
+    Meteor.methods({
+        
+        getRestData(whichData) {
+            
+            let url = 'https://jsonplaceholder.typicode.com/'+whichData;
+            console.log("URL: " + url);
+            let result = '';
+            let statusCode = '';
+            
+            try {
+                try {
+                  let callResult = HTTP.call('GET', url, {});
+                  statusCode = callResult.statusCode;
+                  result = JSON.stringify(callResult);
+                } catch (callErr) {
+                  statusCode = callErr.response.statusCode;
+                  console.log('Catch - Call error:');
+                  // console.log(callErr);
+                  result = '{"error" : "' + callErr.response + '"}';
+                } finally {
+                  console.log('Finally');
+                  console.log('Status Code: ' + statusCode);
+                  // console.log('Result to be returned:');
+                  // console.log(result);
+                  return result;
+                }
+            } catch (err) {
+                console.log('General error');
+                console.log(err);
+                return '{"error" : "' + err + '"}';
+            }
+        }
+    });
+}
