e4121eece80fb7a02c94d13ee02677f6904bfe28
Phil <phil_colbert@hotmail.com>
2018-01-26 10:53:22 -0800

    Update Weather.jsx

--- samples/meteor/p2/imports/ui/Weather.jsx
+++ samples/meteor/p2/imports/ui/Weather.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // Weather component - represents a single weather result
 export default class Weather extends Component {
