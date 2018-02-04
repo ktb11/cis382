13e8bafe13ae18498773321078c3c9bf69a38b47
Phil <phil_colbert@hotmail.com>
2018-02-04 13:03:26 -0800

    Update http.js

--- samples/meteor/httpcall/imports/api/http.js
+++ samples/meteor/httpcall/imports/api/http.js
@@ -1,71 +1,81 @@
-import React, { Component } from 'react';
-import Typicode from './Typicode.js';
+import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {restData: []};
-  }
-    
-  handleButtonClick(event) {
-    
-    // Prevent default submit event - only necessary if you are using forms
-    // event.preventDefault();
-    // console.log("Button: " + event.target.id);
-    let whichData = 'users';
-    this.getRESTData(whichData);
-    
-  }
-  
-  getRESTData(whichData) {
-    // Call server-side method to return data
-    // Why bind(this) below? See http://stackoverflow.com/questions/35581611/exception-in-delivering-result-of-invoking-typeerror-is-not-a-function-in-met
-    // Note that setState is asynchronous, which means you should not immediately (sequentially) depend on a setState changing
-    if (Meteor.isClient) {
-      Meteor.call("getRestData", whichData, function(error, results) {
-        try{
-          if (error) {
-            // Note: We will not be using Meteor error parameter, so it will always be undefined
-            // See http://docs.meteor.com/api/methods.html#Meteor-Error if you want to use Meteor errors
-            console.log("Error in retrieving data: " + error);
-          } else {
-            //let parsedResults = JSON.parse(results);
-            if (results.error) {
-              console.log("Error status code: " + results.error.statusCode);
-              console.log("Error: " + results.error.error);
-            } else {
-              this.setState({restData: JSON.parse(results.content)});
-            }
-          }
-        } catch (err) {
-          console.log("Error in Meteor call: " + err);
+/*
+
+    Three types of results possible within this code:
+    1. Valid HTTP call, will return JSON information as
+        {
+            statusCode: xxx,
+            content: [...] // Note that content is already a JSON string
         }
-      }.bind(this));
+
+    2. Error: Invalid HTTP call
+        {
+            response: 
+                {
+                    statusCode: xxx,
+                    content: { ... },
+                    headers: { ... },
+                }
+        }
+
+    3. Error: Some other type of error
+
+    We will return either the data results, or an object formatted as:
+    {
+        error: {
+            statusCode: xxx,
+            error: '',
+        }
     }
-  }
-  
-  renderRESTData() {
-    if (this.state.restData) {
-      return <Typicode data={this.state.restData} />
-    }
-  }
-  
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <div id="title">REACT REST Calls</div><br />
-          <div id="credit">Powered by <a href="https://jsonplaceholder.typicode.com/">JSONPlaceholder</a></div>
-          <p>
-          Return JSON data using REST call via HTTP package using Meteor and React.
-          </p>
-          <div><button id="getUserData" onClick={this.handleButtonClick.bind(this)}>Get User Data</button></div>
-          
-        </header>
-        <main>
-            {this.renderRESTData()}
-        </main>
-      </div>
-    );
-  }
+
+*/
+
+if (Meteor.isServer) {
+    Meteor.methods({
+        
+        getRestData(whichData) {
+            
+            // Construct URL, and output to console
+            let url = 'https://jsonplaceholder.typicode.com/'+whichData;
+            console.log("URL: " + url);
+
+            let result = '';
+            let statusCode = '';
+
+            try {
+                // Trap non-HTTP errors
+                try {
+                    // Do HTTP call
+                    let callResult = HTTP.call('GET', url);
+                    statusCode = callResult.statusCode;
+                    result = callResult;
+                } catch (callErr) {
+                    // Trap HTTP error
+                    result = {
+                        error: {
+                            statusCode: callErr.response.statusCode,
+                            error: "HTTP error",
+                        }
+                    }
+                    result = JSON.stringify(error);
+                } finally {
+                    // Return HTTP result (valid or invalid)
+                    return result;
+                }
+            } catch (err) {
+                // Handle non-HTTP errors
+                console.log('General error');
+                console.log(err);
+                result = {
+                    error: {
+                        statusCode: 0,
+                        error: err
+                    }
+                }
+                return JSON.stringify(error);
+            }
+        },
+    });
 }
