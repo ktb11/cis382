0b614f3c76c50cd0acf555f997776108b36c9818
Phil <phil_colbert@hotmail.com>
2018-02-04 13:04:16 -0800

    Update Typicode.jsx

--- samples/meteor/httpcall/imports/ui/Typicode.jsx
+++ samples/meteor/httpcall/imports/ui/Typicode.jsx
@@ -4,21 +4,20 @@ import PropTypes from 'prop-types';
 export default class Typicode extends Component {
   render() {
     if (Object.keys(this.props.data).length > 0) {
-        let results = JSON.parse(this.props.data.content);
         // Why an array? Each element is an object, so make an array of objects
         // http://stackoverflow.com/questions/22876978/loop-inside-react-jsx
         let listItems = [];
         // You could use the standard for loop or ES6 arrow function with map
         /*
-        for (let i = 0; i < results.length; i++) {
-            let s = results[i].name;
-            let id = results[i].id;
+        for (let i = 0; i < this.props.data.length; i++) {
+            let s = this.props.data[i].name;
+            let id = this.props.data[i].id;
             // Try without key and see what error you get
             listItems.push(<li key={id}>{s}</li>);
         }
         */
         // ES6 arrow function uses less code
-        results.map(user => listItems.push(<li key={user.id}>{user.name}</li>));
+        this.props.data.map(user => listItems.push(<li key={user.id}>{user.name}</li>));
         return (
             <div>
             <ul>
@@ -35,5 +34,5 @@ export default class Typicode extends Component {
 Typicode.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
-  data: PropTypes.object.isRequired,
+  data: PropTypes.array.isRequired,
 };
